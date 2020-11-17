cc7d9038f6c8010044079b5c47c1ddbb127585e3
Filipe-Santos511 <filipesantos511@gmail.com>
2020-11-17 17:29:37 +0000

    teste

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -5,8 +5,15 @@
 
 int main()
 {
+  unsigned int n1=1, n2=1, n3, i;
   //init uart 
   uart_init(UART_BASE,FREQ/BAUD);   
-  uart_printf("\n\n\nHello world!\n\n\n");
-  
+  uart_printf("\n1:1 \n 2:1 \n");
+  for(i=3;i<101;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  uart_printf("%d: %d\n", i, n3);    
+  n1=n2;    
+  n2=n3;    
+ }  
 }
